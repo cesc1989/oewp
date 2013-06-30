186317e262717b49217c6675d323fd4dba7340e5
Francisco Quintero <frajquico@yahoo.es>
2013-06-30 00:12:46 -0500

    anadido oembed

---
+++ php/subida_scribd/colecta_enlace.php
@@ -0,0 +1,8 @@
+<?php session_start(); ?>
+<body>
+	<div id="scribd"><?php echo $_SESSION['scribd']; ?></div>
+</body>
+<?php echo '<head>
+		<meta http-equiv=refresh content=1;subida_scribd.html>
+	</head>';
+?>
---
+++ php/subida_scribd/sube_scribd.php
@@ -0,0 +1,27 @@
+<?php session_start();
+
+require_once 'scribd.php';
+
+$scribd_api_key = "5bdi42g6bioa3a7tz9urh";
+$scribd_secret = "sec-8mskuhnxkwnv96ajgrz9yaogt8"; 
+
+$scribd = new Scribd($scribd_api_key, $scribd_secret);
+if ($scribd){
+	$url_scribd = "http://scribd.com/doc/";
+	$file = $_FILES['userfile']['tmp_name'];
+	$doc_type = null;
+	$access = null;
+	$rev_id = null;
+	$data = $scribd->upload($file, $doc_type, $access, $rev_id); // returns Array ( [doc_id] => 1026598 [access_key] => key-23nvikunhtextwmdjm2i )
+	print_r($data);
+	echo "<br />";
+	$enlace_pegable = $url_scribd.$data[doc_id];
+	$_SESSION['scribd'] = $enlace_pegable;
+
+	print "<div id=prueba>".$_SESSION['scribd']."</div>";
+
+	echo '<head>
+			<meta http-equiv=refresh content=0;subida_scribd.html>
+		</head>';
+};
+?>
